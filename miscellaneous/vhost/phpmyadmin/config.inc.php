55ccdb997105efec8289b1e2228c5e3293726ed7
Mesaque S. Silva <mesaque.silva@apiki.com>
2018-04-03 15:12:55 -0300

    automatic login phpmyadmin

--- miscellaneous/vhost/phpmyadmin/config.inc.php
+++ miscellaneous/vhost/phpmyadmin/config.inc.php
@@ -30,7 +30,11 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'db';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = 'root';
 
 /**
  * phpMyAdmin configuration storage settings.
