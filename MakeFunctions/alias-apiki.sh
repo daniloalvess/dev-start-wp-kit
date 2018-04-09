9c5b656a8a0e299cc3402073e66f66fe2ce2b3ca
Elvis Henrique Pereira <elvishp2006@gmail.com>
2018-04-09 10:46:21 -0300

    Update alias-apiki.sh

--- MakeFunctions/alias-apiki.sh
+++ MakeFunctions/alias-apiki.sh
@@ -1,11 +1,69 @@
-composer () {
-   docker exec -it --user $(id -u):$(id -g) --env PWD=${PWD} php7 bash -c "composer $argv"
+php () {
+  docker exec \
+    -i \
+    php7 \
+    bash \
+    -c "php $argv"
 }
 
-node () {
-   docker run -it --rm --user $(id -u):$(id -g) --env PWD=${PWD} --volumes-from disk --volume /etc/passwd:/etc/passwd --volume /etc/group:/etc/group devstartwpkit_node:latest bash -c "cd $PWD && node $argv"
+composer () {
+   docker exec \
+    -it \
+    --user (id -u):(id -g) \
+    --env PWD=$PWD \
+    php7 \
+    bash \
+    -c "cd $PWD && composer $argv" 
+}
+
+phpcs () {
+   docker exec \
+    -it \
+    --user (id -u):(id -g) \
+    --env PWD=$PWD \
+    php7 \
+    bash \
+    -c "cd $PWD && phpcs $argv" 
+}
+
+mysql () {
+  docker exec \
+    -it \
+    mysql \
+    mysql -h127.0.0.1 -proot $argv
+}
+
+mysqlimport () {
+  docker exec \
+    -i \
+    mysql \
+    mysql -h127.0.0.1 -proot $argv
 }
 
 yarn () {
-   docker run -it --rm --user $(id -u):$(id -g) --env PWD=${PWD} --volumes-from disk --volume /etc/passwd:/etc/passwd --volume /etc/group:/etc/group devstartwpkit_node:latest bash -c "cd $PWD && yarn $argv"
+   docker run \
+     -it \
+     --rm \
+     --user (id -u):(id -g) \
+     --env PWD=$PWD \
+     --volumes-from disk \
+     --volume /etc/passwd:/etc/passwd \
+     --volume /etc/group:/etc/group \
+     devstartwpkit_node:latest \
+     bash \
+     -c "cd $PWD && yarn $argv"
+}
+
+npm () {
+   docker run \
+     -it \
+     --rm \
+     --user (id -u):(id -g) \
+     --env PWD=$PWD \
+     --volumes-from disk \
+     --volume /etc/passwd:/etc/passwd \
+     --volume /etc/group:/etc/group \
+     devstartwpkit_node:latest \
+     bash \
+     -c "cd $PWD && npm $argv"
 }
