acda77e9d033962a74355ef42ef9c4c4db851cfc
Mesaque S. Silva <mesaque.silva@apiki.com>
2018-04-11 09:20:32 -0300

    fix mysql install

--- MakeFunctions/docker-fix.sh
+++ MakeFunctions/docker-fix.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
+
+if [ ! -d "/var/lib/mysql" ]; then
+	sudo tar -xzf ./miscellaneous/mysql/mysql-data.tgz -C /var/lib
+fi
+
+
 docker-compose up --detach
 docker exec php7 /bin/bash -c "sed -i.bak s/www-data/$(whoami)/g /usr/local/etc/php-fpm.d/www.conf"
 docker exec php6 /bin/bash -c "sed -i.bak s/www-data/$(whoami)/g /usr/local/etc/php-fpm.d/www.conf"
