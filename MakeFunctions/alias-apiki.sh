f5f596f1b127c9b38ddca44aad6ac980b04bf31e
Mesaque S. Silva <mesaque.silva@apiki.com>
2018-04-23 13:37:20 -0300

    add nom and yarn commands

--- MakeFunctions/alias-apiki.sh
+++ MakeFunctions/alias-apiki.sh
@@ -13,33 +13,3 @@ mysqlimport () {
     mysql \
     mysql -h127.0.0.1 -proot $argv
 }
-
-yarn () {
-   docker run \
-     -it \
-     --rm \
-     --user $(id -u):$(id -g) \
-     --env PWD=$PWD \
-     --volumes-from disk \
-     --volume /etc/passwd:/etc/passwd \
-     --volume /etc/group:/etc/group \
-     -w $PWD \
-     apiki/node:1.1 \
-     bash \
-     -c "yarn $argv"
-}
-
-npm () {
-   docker run \
-     -it \
-     --rm \
-     --user $(id -u):$(id -g) \
-     --env PWD=$PWD \
-     --volumes-from disk \
-     --volume /etc/passwd:/etc/passwd \
-     --volume /etc/group:/etc/group \
-     -w $PWD \
-     apiki/node:1.1 \
-     bash \
-     -c "npm $argv"
-}
